dd60a89bb9f20645cd24368600f38dcb84fae636
Mauricio Arce Torrez <mauricio.arce.mat@gmail.com>
2024-10-17 21:56:41 -0400

    Update web view validation

--- src/app/core/utils/is-web-view.utils.ts
+++ src/app/core/utils/is-web-view.utils.ts
@@ -1,17 +1,7 @@
 export const isWebView = () => {
   const userAgent = navigator.userAgent;
 
-  // Check for common webview patterns
-  const webViewPatterns = [
-    /FBAN/, // Facebook
-    /FBAV/, // Facebook
-    /Instagram/, // Instagram
-    /Twitter/, // Twitter
-    /WebView/, // Generic WebView
-    /wv/, // Android WebView
-    /iPhone.*Mobile.*Safari/, // iOS WebView
-    /Android.*Version\/[\d.]+.*Chrome\/[\d.]+.*Mobile Safari\/[\d.]+/, // Android WebView
-  ];
-
-  return webViewPatterns.some((pattern) => pattern.test(userAgent));
+  return /FBAN|FBAV|Instagram|Twitter|Line|Snapchat|WhatsApp|Messenger/i.test(
+    userAgent,
+  );
 };
