d918b71d1055ae9d72699f646fc37e5604208865
Mauricio Arce Torrez <mauricio.arce.mat@gmail.com>
2024-10-20 22:35:28 -0400

    Update candeactivate guard message

--- src/app/core/guards/exit-registration.guard.ts
+++ src/app/core/guards/exit-registration.guard.ts
@@ -10,7 +10,7 @@ export const exitRegistrationGuard: CanDeactivateFn<{
   const message =
     registrationStep === RegistrationStep.form
       ? 'No completaste tu registro, ¿estás seguro de querer salir?'
-      : 'Tu pago se está procesando, ¿estás seguro de querer salir? Si Wolipay indica que tu pago fue exitoso, no te preocupes, tu inscripción se completó correctamente.';
+      : 'Tu pago se está procesando, ¿estás seguro de querer salir? Si Wolipay indica que tu pago fue exitoso, no te preocupes, tu registro se completó correctamente.';
 
   return component.canDeactivate() ? true : confirm(message);
 };
