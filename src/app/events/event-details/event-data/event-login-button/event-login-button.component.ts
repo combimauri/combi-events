b36338113228d0012ff83cbf49154b46d50403e2
Mauricio Arce <mauricio.arce.mat@gmail.com>
2025-09-12 00:18:29 +0200

    Avoid redirect to registration page if event is not listed

--- src/app/events/event-details/event-data/event-login-button/event-login-button.component.ts
+++ src/app/events/event-details/event-data/event-login-button/event-login-button.component.ts
@@ -7,6 +7,7 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { EventState } from '@core/states';
 
 @Component({
   selector: 'combi-event-login-button',
@@ -26,13 +27,18 @@ import { Router } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EventLoginButtonComponent {
-  readonly eventId = input.required<string>();
-
+  readonly #eventState = inject(EventState);
   readonly #router = inject(Router);
+
+  readonly eventId = input.required<string>();
 
   navigateToLogin(): void {
     const eventId = this.eventId();
-    const returnUrl = `/${eventId}/register`;
+    const event = this.#eventState.event;
+    // If event is not listed, registration is allowed, but it's hidden
+    const returnUrl = event()?.listEvent
+      ? `/${eventId}/register`
+      : `/${eventId}`;
 
     this.#router.navigate(['/login'], {
       queryParams: { returnUrl },
