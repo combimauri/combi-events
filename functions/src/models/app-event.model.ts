9bfa77fd8275c1b8ec57d213124e041193a6c1fa
Mauricio Arce <mauricio.arce.mat@gmail.com>
2025-11-06 01:46:01 +0100

    Send email when payment is validated.

--- functions/src/models/app-event.model.ts
+++ functions/src/models/app-event.model.ts
@@ -1,12 +1,17 @@
+import { Timestamp } from 'firebase-admin/firestore';
 import { Price } from './price.model';
 
 export type AppEvent = {
-  id: string
+  id: string;
+  bannerImage: string;
   capacity: number;
   count: number;
   maxSessionsPerUser: number;
   name: string;
+  description: string;
   owner: string;
   price: Price;
   openRegistration: boolean;
+  date: { end: Timestamp; start: Timestamp };
+  location: { name: string };
 };
